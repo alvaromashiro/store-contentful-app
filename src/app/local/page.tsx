38616d497b4782dd7469edc92e1de2ab1d0d09d9
alvaromashiro <alvaromashiro@gmail.com>
2023-08-01 15:06:58 -0600

    Update helper implementations to query page

--- src/app/local/page.tsx
+++ src/app/local/page.tsx
@@ -2,22 +2,21 @@ import { getEntriesByContentType, getFields } from "@/lib/helpers";
 
 const getData = async () => {
   const entries = await getEntriesByContentType("landingPage");
-  console.log(entries);
-
   return entries;
 };
 
 const LocalesPage = async () => {
   const data = await getData();
-  const fields = getFields(data);
-  const moreFields = getFields(fields);
-  // console.log(data);
+  if (!data) {
+    return null;
+  }
+  const fields = getFields(data[0]);
+  const moreFields = getFields(fields.sections);
 
   return (
     <div>
       <h1>{moreFields.title}</h1>
       <p>{moreFields.subtitle}</p>
-      <pre>{JSON.stringify(moreFields, null, 2)}</pre>
     </div>
   );
 };
